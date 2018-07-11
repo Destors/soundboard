4db5a696ba88821b2f308eb1814853110f4f2564
Alex <halen54001@gmail.com>
2018-07-11 22:44:18 +0300

    start_0.3-simpled

--- js/script.js
+++ js/script.js
@@ -1,46 +1,20 @@
-(function() {
-  
-  function keyDownHandler(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    playSound(audio, key);
-  }
-  
-  function clickHandler(e) {
-    if (this.dataset.key) {
-      const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
-      const key = document.querySelector(`div[data-key="${this.dataset.key}"]`);
-      playSound(audio, key);
-    } else { 
-      const audio = document.querySelector(`audio#${this.dataset.target}`);
-      const key = document.querySelector(`div[data-target="${this.dataset.target}"]`);
-      playSound(audio, key);
-    }
-    
-  }
-  
-  function playSound(audio, key) {    
-    if (!audio) return;
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
-  
-  function removeTransition(e) {
-    e.target.classList.remove('playing');
-  }
-  
-  const keys = Array.from(document.querySelectorAll('.sounbord__btn-wrapper'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', keyDownHandler);
-  
-  keys.forEach(key => key.addEventListener('click', clickHandler));
-  
-})();
+// Вариант один
+document.addEventListener('keydown', function (e) {
+  const audio = document.querySelector('audio[data-key="'+e.keyCode+'"]');
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+});
 
-/*var playSound = window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-  });*/
+//вариант два
+/*
+function keyDownTracker(e) {
+  const audio = document.querySelector('audio[data-key="'+e.keyCode+'"]');
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.sounbord__btn-wrapper'));
+window.addEventListener('keydown', keyDownTracker);
+*/
